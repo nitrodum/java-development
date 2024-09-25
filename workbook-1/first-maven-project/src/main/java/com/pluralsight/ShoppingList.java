ec7652507d75e985527fbe485efc7cf93975aabf
Nima_Lama <nitrodum@yahoo.com>
2024-09-25 11:07:05 -0700

    created shopping list

---
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class ShoppingList {
+
+    public static void main(String[] args) {
+        String[] list = {"Milk", "Eggs", "Bread", "Chips", "Waffles", "Peanut Butter", "Sausage", "Jelly", "Nuts", "Ice Cream"};
+        for (String item : list) {
+            System.out.println(item);
+        }
+    }
+}
