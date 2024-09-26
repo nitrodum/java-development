942e2c5b6e1dfe9c4d96b5f11ef5f77b96a5ef73
Nima_Lama <nitrodum@yahoo.com>
2024-09-26 15:18:31 -0700

    Completed Mod3 - Exercise 4 - Payroll Calculator

---
+++ workbook-1/PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator.java
@@ -0,0 +1,43 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class PayrollCalculator {
+    public static void main(String[] args) {
+        String name;
+        float hoursWorked = 0.0f;
+        float payRate = 0.0f;
+        boolean isValid = false;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your name: ");
+        name = scanner.nextLine();
+
+        do {
+            System.out.println("Please enter your hours worked: ");
+            if (scanner.hasNextFloat()) {
+                hoursWorked = scanner.nextFloat();
+                isValid = true;
+            } else {
+                System.out.println("Not a valid entry, please reenter a valid number");
+                scanner.next();
+            }
+        } while (!isValid);
+
+        isValid = false;
+        do {
+            System.out.println("Please enter your pay rate: ");
+            if (scanner.hasNextFloat()) {
+                payRate = scanner.nextFloat();
+                isValid = true;
+            } else {
+                System.out.println("Not a valid entry, please reenter a valid number");
+                scanner.next();
+            }
+        } while (!isValid);
+        float grossPay = payRate*hoursWorked;
+
+        System.out.printf("Hello, %s your gross pay is $%.2f", name, grossPay);
+        scanner.close();
+    }
+}
