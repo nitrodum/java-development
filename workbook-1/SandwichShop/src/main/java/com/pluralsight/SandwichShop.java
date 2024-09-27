a7e04cb2c7f306e393c125c5c3f3c83197e9e0f6
Nima_Lama <nitrodum@yahoo.com>
2024-09-27 15:36:32 -0700

    Added Loaded option for the sandwich shop

--- workbook-1/SandwichShop/src/main/java/com/pluralsight/SandwichShop.java
+++ workbook-1/SandwichShop/src/main/java/com/pluralsight/SandwichShop.java
@@ -10,11 +10,20 @@ public class SandwichShop {
                 "Type 1 for a regular sized sandwich or 2 for a large sandwich."
                 );
         String size = scanner.nextLine();
+        System.out.println("Would you like to have the sandwich loaded?\n" +
+                "Please respond yes or no: ");
+        String loaded = scanner.nextLine();
         float price = 0.0f;
         if (size.equals("1")) {
             price = 5.45f;
+            if (loaded.equals("yes")){
+                price += 1f;
+            }
         } else if (size.equals("2")) {
             price = 8.95f;
+            if (loaded.equals("yes")){
+                price += 1.75f;
+            }
         }
 
         System.out.println("What is your age?");
@@ -28,5 +37,6 @@ public class SandwichShop {
         }
 
         System.out.printf("Your total cost is: $%.2f", price);
+        scanner.close();
     }
 }
