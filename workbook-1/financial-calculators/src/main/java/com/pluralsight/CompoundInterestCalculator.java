72a6a21a3501ea85c6da3d833562fc0e3ac9ce53
Nima_Lama <nitrodum@yahoo.com>
2024-09-30 15:02:57 -0700

    Finished the CompoundingInterestCalculator and got it to output future value and total interest earned

--- workbook-1/financial-calculators/src/main/java/com/pluralsight/CompoundInterestCalculator.java
+++ workbook-1/financial-calculators/src/main/java/com/pluralsight/CompoundInterestCalculator.java
@@ -7,12 +7,21 @@ public class CompoundInterestCalculator {
     public static void main(String[] args) {
         System.out.println("Welcome to the Compound Interest Calculator!");
         double p = input("deposit");
-        double r = input("interest rate")/100/12;
-        double n = input("number of years")*12;
+        double r = input("interest rate")/100;
+        double t = input("number of years");
+        double n = 365;
+
+        double A = getFutureValue(p, r, t, n);
+        System.out.printf("The future value of this account is: $%.2f\n", A);
+        System.out.printf("You will earn $%.2f in interest!", (A-p));
     }
 
     public static double input(String message){
-        System.out.println("Please enter the ");
+        System.out.println("Please enter the " + message);
         return scanner.nextDouble();
+    }
+
+    public static double getFutureValue(double p, double r, double t, double n) {
+        return (p * Math.pow((1+(r/n)),(n*t)));
     }
 }
