53932209a7de55435a6bac8471b11fe17a1eddbf
Nima_Lama <nitrodum@yahoo.com>
2024-09-30 15:28:06 -0700

    got AnnnuityCalculator to ouptut annuity

--- workbook-1/financial-calculators/src/main/java/com/pluralsight/AnnuityCalculator.java
+++ workbook-1/financial-calculators/src/main/java/com/pluralsight/AnnuityCalculator.java
@@ -6,13 +6,20 @@ public class AnnuityCalculator {
     static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("Welcome to the Annuity Calculator!");
-        double m = input("monthly payout");
-        double r = input("interest rate");
-        double n = input("years to pay out");
+        double p = input("monthly payout");
+        double r = input("interest rate")/100/12;
+        double t = input("years to pay out")*12;
+
+        double pv = getPresentValue(p, r, t);
+        System.out.printf("The present value for this annuity is: $%.2f", pv);
     }
 
     public static double input(String message) {
         System.out.println("Please enter the " + message);
         return scanner.nextDouble();
+    }
+
+    public static double getPresentValue(double p, double r, double t) {
+        return  p * (1 - Math.pow((1+r),-t)) / r ;
     }
 }
