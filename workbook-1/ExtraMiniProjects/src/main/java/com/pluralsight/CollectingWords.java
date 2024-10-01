044ac3a0631e82e3e356bc9f3fe01087f3a4191f
Nima_Lama <nitrodum@yahoo.com>
2024-10-01 10:27:52 -0700

    Finished while loop to take in inputs and display

--- workbook-1/ExtraMiniProjects/src/main/java/com/pluralsight/CollectingWords.java
+++ workbook-1/ExtraMiniProjects/src/main/java/com/pluralsight/CollectingWords.java
@@ -5,12 +5,21 @@ import java.util.Scanner;
 
 public class CollectingWords {
     static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         ArrayList<String> wordList = new ArrayList<>();
         String word;
         System.out.println("Please enter a word.");
         while (!(word = scanner.nextLine()).isBlank()) {
             wordList.add(word);
+            for (int i = 0; i < wordList.size(); i++) {
+                if (i == 0) {
+                    System.out.print(wordList.get(i));
+                } else {
+                    System.out.print(", " + wordList.get(i));
+                }
+            }
+            System.out.println();
         }
     }
 }
