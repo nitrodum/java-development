52bd705eebb51cbda32e7d6b931dc93e1ee17b67
Nima_Lama <nitrodum@yahoo.com>
2024-10-01 09:58:49 -0700

    Did the bonus for GuessTheNumber

--- workbook-1/ExtraMiniProjects/src/main/java/com/pluralsight/GuessTheNumber.java
+++ workbook-1/ExtraMiniProjects/src/main/java/com/pluralsight/GuessTheNumber.java
@@ -2,56 +2,26 @@ package com.pluralsight;
 
 import java.util.LinkedList;
 import java.util.Scanner;
+import java.io.File;
 import java.io.FileWriter;
-import java.io. IOException;
+import java.io.IOException;
 
 public class GuessTheNumber {
-    static LinkedList<Integer> guesses = new LinkedList<>();
     static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
+        double totalGuesses = 0;
+        double gamesToRun = 10000;
 
-        int random = (int)(Math.random()*10) + 1;
-        int guess = 0;
-        int count = 1;
-
-
-        System.out.println("Guess a number between 1-10");
-        guess = getGuess();
-
-        if (guess == random) {
-            //System.out.println("You got the right number " + random + " on the first try!");
-        } else {
-            while (guess != random) {
-                System.out.println("Guess again");
-                guess = getGuess();
-                count++;
-            }
-
-            //System.out.println("You guessed the right number " + random + " in " + count + " tries!");
-
+        for (int i = 0; i < gamesToRun; i++) {
+            totalGuesses += runScript();
         }
-        try {
-            FileWriter writer = new FileWriter("results.txt");
-            writer.write("You guessed the right number " + random + " in " + count + " guesses.\n");
-            writer.write("Your guesses: ");
-            for (Integer number : guesses) {
-                writer.write(number + " ");  // Convert integer to string and add a space between guesses
-            }
-            writer.close(); // Remember to close the FileWriter after writing
-            System.out.println("Results written to file.");
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
+        System.out.printf("The average number of guesses was %.2f",(totalGuesses / gamesToRun));
         scanner.close();
-        /*System.out.println("Your guesses: ");
-        for (Integer number : guesses) {
-            System.out.println(number);
-        }
-        */
+
     }
 
-    public static int getGuess() {
+    public static int getGuess(LinkedList<Integer> guesses) {
         int guess = -1;
         boolean isValid = false;
         do {
@@ -64,5 +34,63 @@ public class GuessTheNumber {
             }
         } while (!isValid);
         return guess;
+    }
+
+    public static void runGame() {
+        int random = (int) (Math.random() * 10) + 1;
+        int guess = 0;
+        int count = 1;
+        LinkedList<Integer> guesses = new LinkedList<>();
+
+        System.out.println("Guess a number between 1-10");
+        guess = getGuess(guesses);
+
+        if (guess == random) {
+            System.out.println("You got the right number " + random + " on the first try!");
+        } else {
+            while (guess != random) {
+                System.out.println("Guess again");
+                guess = getGuess(guesses);
+                count++;
+            }
+
+            System.out.println("You guessed the right number " + random + " in " + count + " tries!");
+            if (count < 5) {
+                System.out.println("You are awesome!");
+            }
+
+        }
+    }
+
+    public static int runScript() {
+        int random = (int) (Math.random() * 10) + 1;
+        int guess = 0;
+        int count = 1;
+        LinkedList<Integer> guesses = new LinkedList<>();
+        guess = (int) (Math.random() * 10) + 1;
+        guesses.add(guess);
+        while (guess != random) {
+            guess = (int) (Math.random() * 10) + 1;
+            guesses.add(guess);
+            count++;
+        }
+        writeToFile(guesses, count, random);
+        return count;
+    }
+
+    public static void writeToFile(LinkedList<Integer> guesses, int count, int random) {
+        try {
+            FileWriter writer = new FileWriter("results.txt", true);
+            writer.write("You guessed the right number " + random + " in " + count + " guesses.\n");
+            writer.write("Your guesses: ");
+            for (Integer number : guesses) {
+                writer.write(number + " ");
+            }
+            writer.write("\n");
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 }
