d49edc8a19823c1769ecffb592e256b3da6a7f6b
Nima_Lama <nitrodum@yahoo.com>
2024-11-22 13:32:30 -0800

    added more secure method of sql

--- workbook-8/NorthwindTraders/src/main/java/com/pluralsight/Main.java
+++ workbook-8/NorthwindTraders/src/main/java/com/pluralsight/Main.java
@@ -1,22 +1,47 @@
 package com.pluralsight;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "root", "yearup");
 
-        Statement statement = connection.createStatement();
+        String query = "SELECT ProductID, ProductName FROM products";
 
-        String query = "SELECT * FROM products";
+        PreparedStatement statement = connection.prepareStatement(query);
 
         ResultSet results = statement.executeQuery(query);
 
         while (results.next()) {
+            int id = results.getInt("ProductID");
             String name = results.getString("ProductName");
-            System.out.println(name);
+            System.out.println(id + " " + name);
         }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the id of the product you would like get details for: ");
+        int productID = scanner.nextInt();
+        scanner.nextLine();
+
+        query = "SELECT ProductID, ProductName FROM products WHERE ProductID = ?";
+
+        statement = connection.prepareStatement(query);
+        statement.setInt(1, productID);
+
+        results = statement.executeQuery();
+
+        while (results.next()) {
+            int id = results.getInt("ProductID");
+            String name = results.getString("ProductName");
+            System.out.println(id + " " + name);
+        }
+
+        results.close();
+        statement.close();
         connection.close();
+
+        scanner.close();
     }
 }
