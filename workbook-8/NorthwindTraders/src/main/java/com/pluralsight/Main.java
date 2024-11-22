f1e7fef82835e6194c31cae827b955c90480dc97
Nima_Lama <nitrodum@yahoo.com>
2024-11-22 14:57:50 -0800

    Completed JDBC Exercise 2

--- workbook-8/NorthwindTraders/src/main/java/com/pluralsight/Main.java
+++ workbook-8/NorthwindTraders/src/main/java/com/pluralsight/Main.java
@@ -4,44 +4,48 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "root", "yearup");
-
-        String query = "SELECT ProductID, ProductName FROM products";
-
-        PreparedStatement statement = connection.prepareStatement(query);
-
-        ResultSet results = statement.executeQuery(query);
-
-        while (results.next()) {
-            int id = results.getInt("ProductID");
-            String name = results.getString("ProductName");
-            System.out.println(id + " " + name);
+    private static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws SQLException {
+        try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", args[0], args[1])) {
+            getAllProducts(connection);
+            getProductByID(connection);
         }
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the id of the product you would like get details for: ");
-        int productID = scanner.nextInt();
-        scanner.nextLine();
-
-        query = "SELECT ProductID, ProductName FROM products WHERE ProductID = ?";
-
-        statement = connection.prepareStatement(query);
-        statement.setInt(1, productID);
-
-        results = statement.executeQuery();
-
-        while (results.next()) {
-            int id = results.getInt("ProductID");
-            String name = results.getString("ProductName");
-            System.out.println(id + " " + name);
-        }
-
-        results.close();
-        statement.close();
-        connection.close();
-
         scanner.close();
     }
+
+    private static void getAllProducts(Connection connection) throws SQLException {
+        try(PreparedStatement statement = connection.prepareStatement("SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM products");
+            ResultSet results = statement.executeQuery()
+        ) {
+            printProducts(results);
+        }
+    }
+
+    private static void getProductByID(Connection connection) throws SQLException {
+        System.out.println("Enter the Product ID you would like to search for: ");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+        PreparedStatement statement = connection.prepareStatement("""
+                SELECT ProductID, ProductName, UnitPrice, UnitsInStock
+                FROM products
+                WHERE ProductID = ?""" );
+        statement.setInt(1, id);
+        ResultSet results = statement.executeQuery();
+        printProducts(results);
+        results.close();
+        statement.close();
+    }
+
+    private static void printProducts(ResultSet results) throws SQLException {
+        System.out.printf("%-5s%-35s%-8s%-7s\n", "Id", "Name", "Price", "Stock");
+        System.out.printf("%-5s%-35s%-8s%-7s\n", "-".repeat(4), "-".repeat(34), "-".repeat(7), "-".repeat(6));
+        while (results.next()) {
+            int id = results.getInt("ProductID");
+            String name = results.getString("ProductName");
+            double price = results.getDouble("UnitPrice");
+            int stock = results.getInt("UnitsInStock");
+            System.out.printf("%-5d%-35s%-8.2f%-7d\n", id, name, price, stock);
+        }
+    }
+
 }
