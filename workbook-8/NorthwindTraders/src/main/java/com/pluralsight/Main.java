fa33f68124239c7ce11f425b631667d85d7417f6
Nima_Lama <nitrodum@yahoo.com>
2024-11-25 14:11:04 -0800

    Exercises 3 and 4

--- workbook-8/NorthwindTraders/src/main/java/com/pluralsight/Main.java
+++ workbook-8/NorthwindTraders/src/main/java/com/pluralsight/Main.java
@@ -5,13 +5,38 @@ import java.util.Scanner;
 
 public class Main {
     private static Scanner scanner = new Scanner(System.in);
-    public static void main(String[] args) throws SQLException {
+    private static boolean running = true;
+
+    public static void main(String[] args) {
         try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", args[0], args[1]);
         ) {
-            getAllProducts(connection);
-            getProductByID(connection);
+            while (running) {
+                menu(connection);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         scanner.close();
+    }
+
+    private static void menu(Connection connection) throws SQLException {
+        System.out.println("""
+                --------------------------------------------------------------------------------------------------------
+                What do you want to do?
+                1) Display all products
+                2) Display all customers
+                3) Display all categories
+                0) Exit
+                """);
+        String ans = scanner.nextLine();
+
+        switch (ans) {
+            case "1" -> getAllProducts(connection);
+            case "2" -> getAllCustomers(connection);
+            case "3" -> getAllCategories(connection);
+            case "0" -> running = false;
+            default -> System.out.println("Invalid Option");
+        }
     }
 
     private static void getAllProducts(Connection connection) throws SQLException {
@@ -19,6 +44,38 @@ public class Main {
             ResultSet results = statement.executeQuery()
         ) {
             printProducts(results);
+        }
+    }
+
+    private static void getAllCustomers(Connection connection) throws SQLException {
+        try(PreparedStatement statement = connection.prepareStatement("SELECT ContactName, CompanyName, City, Country, Phone FROM customers");
+            ResultSet results = statement.executeQuery()
+        ) {
+            printCustomers(results);
+        }
+    }
+
+    private static void getAllCategories(Connection connection) throws SQLException {
+        try(PreparedStatement statement = connection.prepareStatement("SELECT CategoryID, CategoryName FROM categories");
+            ResultSet results = statement.executeQuery()
+        ) {
+            printCategories(results);
+        }
+        productByCategory(connection);
+    }
+
+    private static void productByCategory(Connection connection) throws SQLException {
+        System.out.println("\nEnter the category ID you would like to search for: ");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+        try(PreparedStatement statement = connection.prepareStatement("""
+                SELECT ProductID, ProductName, UnitPrice, UnitsInStock
+                FROM products
+                WHERE CategoryID = ?""" )){
+            statement.setInt(1, id);
+            try(ResultSet results = statement.executeQuery()) {
+                printProducts(results);
+            }
         }
     }
 
@@ -46,6 +103,29 @@ public class Main {
             double price = results.getDouble("UnitPrice");
             int stock = results.getInt("UnitsInStock");
             System.out.printf("%-5d%-35s%-8.2f%-7d\n", id, name, price, stock);
+        }
+    }
+
+    private static void printCustomers(ResultSet results) throws SQLException {
+        System.out.printf("%-30s%-40s%-15s%-15s%-13s\n", "ContactName", "CompanyName", "City", "Country", "Phone");
+        System.out.printf("%-30s%-40s%-15s%-15s%-13s\n", "-".repeat(29), "-".repeat(39), "-".repeat(14), "-".repeat(14), "-".repeat(12));
+        while (results.next()) {
+            String contactName = results.getString("ContactName");
+            String companyName = results.getString("CompanyName");
+            String city = results.getString("City");
+            String country = results.getString("Country");
+            String phone = results.getString("Phone");
+            System.out.printf("%-30s%-40s%-15s%-15s%-13s\n", contactName, companyName, city, country, phone);
+        }
+    }
+
+    private static void printCategories(ResultSet results) throws SQLException {
+        System.out.printf("%-11s%-13s\n", "CategoryID", "CategoryName");
+        System.out.printf("%-11s%-13s\n", "-".repeat(10), "-".repeat(12));
+        while (results.next()) {
+            int categoryID = results.getInt("CategoryID");
+            String categoryName = results.getString("CategoryName");
+            System.out.printf("%-11s%-13s\n", categoryID, categoryName);
         }
     }
 
